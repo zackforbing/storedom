1daf5c977a1770aec26a81d173f39e17014b08b6
Zack Forbing <zackforbing@gmail.com>
2016-07-18 14:35:25 -0600

    playing with flashes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,12 @@
 class ItemsController < ApplicationController
   def index
+    # cookies[:hit_counter] = cookies[:hit_counter].to_i + 1
+    session[:hit_counter] = session[:hit_counter].to_i + 1
+    if session[:hit_counter].to_i.even?
+      flash[:alert] = "even!"
+    else
+      flash[:message] = "odd!"
+    end
     @items = Item.all
   end
 
